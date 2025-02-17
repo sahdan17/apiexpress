aa50198e3e41d347aee5b33399a4183250c2ac05
sahdan17 <sahdan.wira.17@gmail.com>
2025-02-17 12:55:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,19 +7,7 @@ const cors = require('cors')
 
 
 
-const allowedOrigins = ['https://apigps.findingoillosses.com']
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}))
+app.use(cors())
 
 const app = express()
 
