c6b6852114c3d1a3900883598ef3238b0f0595f7
sahdan17 <sahdan.wira.17@gmail.com>
2025-02-27 11:54:21 +0700

    Update Associations.js

--- models/Associations.js
+++ models/Associations.js
@@ -1,13 +1,9 @@
 import Driver, { hasMany } from './Driver'
 import DriveSession, { belongsTo } from './DriveSession'
-import Vehicle, { hasMany } from './Vehicle'
-import LastRecord, { belongsTo } from './LastRecord'
 
 const defineAssociations = () => {
     hasMany(DriveSession, { foreignKey: 'driver_id', as: 'driveSessions' })
     belongsTo(Driver, { foreignKey: 'driver_id', as: 'driver' })
-    hasMany(Vehicle, { foreignKey: 'idDevice', as: 'vehicles' })
-    belongsTo(LastRecord, { foreignKey: 'idDevice', as: 'lastRecord' })
 }
 
 export default defineAssociations
