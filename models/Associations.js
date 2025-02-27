fe30a29fc948c2462f35ca2b7cb9f65c80018dfa
sahdan17 <sahdan.wira.17@gmail.com>
2025-02-27 12:04:37 +0700

    Update Associations.js

--- models/Associations.js
+++ models/Associations.js
@@ -1,9 +1,13 @@
 const Driver = require('./Driver')
 const DriveSession = require('./DriveSession')
+const LastRecord = require('./LastRecord')
+const Vehicle = require('./DriveSession')
 
 const defineAssociations = () => {
     Driver.hasMany(DriveSession, { foreignKey: 'driver_id', as: 'driveSessions' })
     DriveSession.belongsTo(Driver, { foreignKey: 'driver_id', as: 'driver' })
+    Vehicle.hasMany(LastRecord, { foreignKey: 'idDevice', as: 'lastRecords' })
+    LastRecord.belongsTo(Vehicle, { foreignKey: 'idDevice', as: 'vehicle' })
 }
 
 module.exports = defineAssociations
