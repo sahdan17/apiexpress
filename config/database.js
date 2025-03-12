05bd1fdf4356192e79503d5815e86dabb8e9d200
sahdan17 <sahdan.wira.17@gmail.com>
2025-03-12 12:08:10 +0700

    u

--- config/database.js
+++ config/database.js
@@ -5,7 +5,16 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
     logging: false,
-    timezone: "+07:00"
+    timezone: "+07:00",
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000, // Waktu maksimum untuk mendapatkan koneksi
+        idle: 10000     // Waktu idle sebelum koneksi dihapus
+    },
+    retry: {
+        max: 5 // Coba ulang koneksi hingga 5 kali jika gagal
+    }
 })
 
 module.exports = sequelize
