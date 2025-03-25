1e1334ba238b9f442f586d662325bdf57340157b
sahdan17 <sahdan.wira.17@gmail.com>
2025-03-25 09:30:38 +0700

    u

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,6 +3,7 @@ const { Op } = require("sequelize")
 const moment = require('moment')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+require('dotenv').config()
 const JWT_SECRET = process.env.JWT_SECRET
 
 function validatePassword(password) {
