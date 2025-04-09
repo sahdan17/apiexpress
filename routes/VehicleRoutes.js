eafd5d4169d0c8c02b826ed6821b496697d52880
sahdan17 <sahdan.wira.17@gmail.com>
2025-04-09 14:33:38 +0700

    u

--- routes/VehicleRoutes.js
+++ routes/VehicleRoutes.js
@@ -9,6 +9,6 @@ router.post('/storeVehicle', authenticateToken, storeVehicle)
 router.post('/getVehicleByCat', authenticateToken, getVehicleByCat)
 router.post('/updateVehicle', authenticateToken, updateVehicle)
 router.post('/deleteVehicle', authenticateToken, deleteVehicle)
-router.post('/getCategory', getCategory)
+router.post('/getCategory', authenticateToken, getCategory)
 
 module.exports = router
