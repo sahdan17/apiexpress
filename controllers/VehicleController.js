6fa7b4eb89fde36b4a81f6597a252ddb988c7ea0
sahdan17 <sahdan.wira.17@gmail.com>
2025-06-02 08:23:21 +0700

    u

--- controllers/VehicleController.js
+++ controllers/VehicleController.js
@@ -1,53 +1,10 @@
 const Vehicle = require('../models/Vehicle')
 const { Op } = require("sequelize")
 const moment = require('moment')
-const Category = require('../models/Category')
 
 exports.getVehicle = async (req, res) => {
     try {
-        const vehicle = await Vehicle.findAll({
-            include: [
-                {
-                    model: Category,
-                    attributes: ['nama_cat'],
-                    as: 'category'
-                }
-            ],
-        })
-
-        const flattened = vehicle.map(v => ({
-            id: v.id,
-            nopol: v.nopol,
-            kode: v.kode,
-            cat: v.cat,
-            category: v.category.nama_cat
-        }))
-
-        res.json(flattened)
-    } catch (error) {
-        res.status(500).send({ message: error.message })
-    }
-}
-
-exports.getCategory = async (req, res) => {
-    try {
-        const category = await Category.findAll()
-
-        res.json(category)
-    } catch (error) {
-        res.status(500).json({ message: error.message })
-    }
-}
-
-exports.getVehicleByCat = async (req, res) => {
-    try {
-        const { cat } = req.body
-
-        const vehicle = await Vehicle.findAll({
-            where: {
-                cat: cat
-            }
-        })
+        const vehicle = await Vehicle.findAll()
 
         res.json(vehicle)
     } catch (error) {
--- routes/VehicleRoutes.js
+++ routes/VehicleRoutes.js
@@ -2,10 +2,8 @@ const express = require('express')
 const { 
     getVehicle, 
     storeVehicle, 
-    getVehicleByCat, 
     updateVehicle, 
     deleteVehicle, 
-    getCategory,
     deleteVehicleInCat 
 } = require('../controllers/VehicleController')
 const authenticateToken = require('../middlewares/AuthMiddleware')
@@ -14,10 +12,8 @@ const router = express.Router()
 
 router.post('/getVehicle', authenticateToken, getVehicle)
 router.post('/storeVehicle', authenticateToken, storeVehicle)
-router.post('/getVehicleByCat', authenticateToken, getVehicleByCat)
 router.post('/updateVehicle', authenticateToken, updateVehicle)
 router.post('/deleteVehicle', authenticateToken, deleteVehicle)
-router.post('/getCategory', authenticateToken, getCategory)
 router.post('/deleteVehicleInCat', authenticateToken, deleteVehicleInCat)
 
 module.exports = router
